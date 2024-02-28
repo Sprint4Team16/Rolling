7b69e21a139c27aa2a237f84e994bd10630e25ed
Hooni07 <gkxogns21@gmail.com>
2024-02-28 21:19:10 +0900

    Fixed: 체크 모양에도 일괄적으로 투명도가 적용되는 문제 해결

--- src/components/post/postmake/ImageSelection.js
+++ src/components/post/postmake/ImageSelection.js
@@ -3,7 +3,14 @@ import CheckIcon from '../../common/CheckIcon';
 
 function ImageSelection({ image, handleFunction, checkSelected }) {
   return (
-    <ImageOption image={image} onClick={handleFunction} selected={checkSelected}>
+    <ImageOption onClick={handleFunction} selected={checkSelected}>
+      <ImageWrapper selected={checkSelected}>
+        <img
+          src={image}
+          alt="배경화면 이미지"
+          style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+        />
+      </ImageWrapper>
       {checkSelected && <CheckIcon imgSrc="img/check.svg" />}
     </ImageOption>
   );
@@ -11,19 +18,29 @@ function ImageSelection({ image, handleFunction, checkSelected }) {
 
 const ImageOption = styled.div`
   position: relative;
-  background: url(${(props) => props.image}) lightgray 50% / cover no-repeat;
-  opacity: ${(props) => (props.selected ? '0.5' : '1')};
   width: 168px;
   height: 168px;
   margin: 5px;
-  border-radius: 16px;
-  border: 1px solid rgba(0, 0, 0, 0.08);
   cursor: pointer;
 
   @media (max-width: 768px) {
     width: 154px;
     height: 154px;
     margin: 12px 0;
+  }
+`;
+
+const ImageWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  border-radius: 16px;
+  border: 1px solid rgba(0, 0, 0, 0.08);
+  background: url(${(props) => props.image}) lightgray 50% / cover no-repeat;
+  opacity: ${(props) => (props.selected ? '0.5' : '1')};
+
+  > img {
+    border-radius: 16px;
   }
 `;
 
