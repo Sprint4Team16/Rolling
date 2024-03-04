5e75bfb10c0ae838e5ca521d827b0b8cc8bb4f5d
Hooni07 <79882248+Hooni07@users.noreply.github.com>
2024-03-04 16:49:31 +0900

    Feat: 롤링페이퍼 삭제 버튼 추가(위치 조정 필요)

--- src/pages/PostId.jsx
+++ src/pages/PostId.jsx
@@ -6,6 +6,7 @@ import Header from '../components/common/Header';
 import SubHeader from '../components/post/SubHeader';
 import Card, { CardContentWrapper } from '../components/post/Card';
 import { getRecipientData } from '../api/GetApi';
+import DeleteRecipientButton from '../components/common/DeleteRecipientButton';
 
 const HeaderWrapper = styled.div`
   position: sticky;
@@ -84,6 +85,7 @@ function PostId({ peopleNum }) {
         <Header />
       </HeaderWrapper>
       <SubHeader name={data ? data.name : 'hello'} peopleNum={peopleNum || 2} />
+      <DeleteRecipientButton />
       <CardWrapper>
         <CardAdd>
           <PlusIcon>
