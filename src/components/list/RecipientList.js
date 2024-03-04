04d45791d69ad6e70392a684ce809d0df4b684e5
김경주 <popo505@naver.com>
2024-03-04 17:38:15 +0900

    Feat: 캐러셀 좌우 버튼추가

--- src/components/list/RecipientList.js
+++ src/components/list/RecipientList.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-// eslint-disable-next-line
+import styled from 'styled-components';
 import RecipientCard from './RecipientCard';
 import { getRecipients } from '../../api/GetApi';
 
@@ -21,12 +21,53 @@ function RecipientList() {
   }, []);
 
   return (
-    <div>
+    <CarouselContainer>
+      <LeftButton>
+        <img src="/img/arrow_left.svg" alt="arrow_left" />
+      </LeftButton>
       {recipients.map((recipient) => (
         <RecipientCard key={recipient.id} recipient={recipient} />
       ))}
-    </div>
+      <RightButton>
+        <img src="/img/arrow_right.svg" alt="arrow_right" />
+      </RightButton>
+    </CarouselContainer>
   );
 }
 
 export default RecipientList;
+
+const CarouselContainer = styled.div`
+  display: flex;
+  position: relative;
+  gap: 20px;
+`;
+
+const Button = styled.button`
+  display: flex;
+  position: absolute;
+  justify-content: center;
+  align-items: center;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 40px;
+  height: 40px;
+  background: #ffffffe5;
+  border-radius: 50px;
+  border: 1px solid #dadcdf;
+  box-shadow: 0px 4px 8px 0px #00000014;
+  cursor: pointer;
+  &:focus {
+    outline: none;
+  }
+`;
+
+const LeftButton = styled(Button)`
+  left: -20px;
+  z-index: 1;
+`;
+
+const RightButton = styled(Button)`
+  right: -20px;
+  z-index: 1;
+`;
