05ec688cd6a6af33b03417bca7b1af0226d7bf67
김경주 <popo505@naver.com>
2024-03-04 19:15:59 +0900

    Feat: RecipientCardList에 캐러셀 기능 추가

--- src/components/list/RecipientCardList.js
+++ src/components/list/RecipientCardList.js
@@ -5,6 +5,7 @@ import { getRecipients } from '../../api/GetApi';
 
 function RecipientList() {
   const [recipients, setRecipients] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   const handleRecipientsLoad = async () => {
     try {
@@ -20,17 +21,31 @@ function RecipientList() {
     handleRecipientsLoad();
   }, []);
 
+  const handleLeftButton = () => {
+    setCurrentIndex((prev) => Math.max(prev - 4, 0));
+  };
+
+  const handleRightButton = () => {
+    setCurrentIndex((prev) => Math.min(prev + 4, recipients.length - 4));
+  };
+
   return (
     <CarouselContainer>
-      <LeftButton>
-        <img src="/img/arrow_left.svg" alt="arrow_left" />
-      </LeftButton>
-      {recipients.map((recipient) => (
-        <RecipientCard key={recipient.id} recipient={recipient} />
-      ))}
-      <RightButton>
-        <img src="/img/arrow_right.svg" alt="arrow_right" />
-      </RightButton>
+      {currentIndex > 0 && (
+        <LeftButton onClick={handleLeftButton}>
+          <img src="/img/arrow_left.svg" alt="arrow_left" />
+        </LeftButton>
+      )}
+      <CardsContainer>
+        {recipients.slice(currentIndex, currentIndex + 4).map((recipient) => (
+          <RecipientCard key={recipient.id} recipient={recipient} />
+        ))}
+      </CardsContainer>
+      {currentIndex < recipients.length - 4 && (
+        <RightButton onClick={handleRightButton}>
+          <img src="/img/arrow_right.svg" alt="arrow_right" />
+        </RightButton>
+      )}
     </CarouselContainer>
   );
 }
@@ -38,6 +53,12 @@ function RecipientList() {
 export default RecipientList;
 
 const CarouselContainer = styled.div`
+  display: flex;
+  position: relative;
+  align-items: center;
+`;
+
+const CardsContainer = styled.div`
   display: flex;
   position: relative;
   gap: 20px;
