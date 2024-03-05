6c67ec94534e2a0070479e61b440b6d3fc7bc07f
Youth-o <2021111495@dgu.ac.kr>
2024-03-05 21:18:14 +0900

    Feat: Modal 공용 Container 추가

--- src/components/post/KakaoModal.jsx
+++ src/components/post/KakaoModal.jsx
@@ -1,5 +1,11 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 // import { useKakaoShare } from './useKakaoShare';
+
+const ModalBox = styled.div`
+  padding: 50px 20px;
+  border-radius: 10px;
+`;
 
 const Mask = styled.div`
   position: fixed;
@@ -14,34 +20,55 @@ const ModalBody = styled.div`
   position: fixed;
   top: 50%;
   left: 50%;
+  width: 600px;
+  height: 478px;
   transform: translate(-50%, -50%);
 `;
 
-const ModalContents = styled.button`
-  background: white;
-  padding: 1rem;
-  width: 400px;
-  height: auto;
-
-  &:hover {
-    background: #f6f6f6;
-  }
+const CloseBtn = styled.button`
+  width: 120px;
+  padding: 7px 16px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 6px;
+  background: var(--Purple-600, #9935ff);
+  font-size: 16px;
+  line-height: 26px; /* 162.5% */
+  letter-spacing: -0.16px;
+  cursor: pointer;
 `;
 
-function KakaoModal() {
+const ModalContents = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: white;
+  padding: 10px;
+  width: 100%;
+  height: 100%;
+  border-radius: 20px;
+`;
+
+function KakaoModal({ children }) {
   console.log('호출됏니?');
+  const [modal, setModal] = useState(true);
+
+  const handleClickBtn = async (e) => {
+    e.preventDefault();
+    setModal((prev) => !prev);
+  };
 
   return (
-    <div className="MyModal">
-      <Mask />
-      <ModalBody>
-        <ModalContents>
-          <h3>모달 타이틀</h3>
-          <p>모달 텍스트 입니다.</p>
-          <button type="submit">닫기</button>
-        </ModalContents>
-      </ModalBody>
-    </div>
+    modal && (
+      <ModalBox>
+        <Mask />
+        <ModalBody>
+          <ModalContents>{children}</ModalContents>
+          <CloseBtn onClick={handleClickBtn}>확인</CloseBtn>
+        </ModalBody>
+      </ModalBox>
+    )
   );
 }
 
