2977be471d70adb618f4dab7c7d47c3ec6b62d40
Hooni07 <79882248+Hooni07@users.noreply.github.com>
2024-03-08 14:32:40 +0900

    Refactor: Primary56 버튼 요소 분리 및 일부 파일 적용

--- src/components/common/Buttons/ButtonStyle.jsx
+++ src/components/common/Buttons/ButtonStyle.jsx
@@ -1,32 +1,65 @@
 import styled from 'styled-components';
 
-const ButtonStyle = styled.button`
+export const ButtonCommon = styled.button`
   display: flex;
-  width: 720px;
-  margin-bottom: 100px;
-  padding: 14px 24px;
   justify-content: center;
-  align-items: center;
-  gap: 10px;
-  border-radius: 12px;
+  gap: 1rem;
   background: var(--${({ disabled }) => (disabled ? 'gray300' : 'purple600')});
-
-  color: var(--white);
   text-align: center;
-  /* Font/18 Bold */
-  font-family: Pretendard;
-  font-size: 18px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 28px; /* 155.556% */
-  letter-spacing: -0.18px;
+`;
 
-  cursor: ${({ disabled }) => disabled && 'not-allowed'};
+const PrimaryBtn = styled(ButtonCommon)`
+  padding: 1.4rem 2.4rem;
 
-  @media (max-width: 768px) {
-    min-width: 320px;
-    width: 100%;
+  // active 여부
+  &:hover {
+    background: var(--purple700);
+  }
+
+  &:active {
+    background: var(--purple800);
+  }
+
+  &:focus {
+    /* border: 2px solid var(--purple900); */
+    background: var(--purple800);
   }
 `;
 
-export default ButtonStyle;
+export const Primary56 = styled(PrimaryBtn)`
+  padding: 1.4rem 2.4rem;
+  border-radius: 1.2rem;
+  background: var(--${({ disabled }) => (disabled ? 'gray300' : 'purple600')});
+  /* Font/18 Bold */
+  color: var(--white);
+
+  /* Font/18 Bold */
+  font-family: Pretendard;
+  font-size: 1.8rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 2.8rem; /* 155.556% */
+  letter-spacing: -0.18px;
+`;
+
+export const Primary40 = styled(PrimaryBtn)`
+  border-radius: 0.6rem;
+  background: var(--${({ disabled }) => (disabled ? 'gray300' : 'purple600')});
+  /* Font/16 Regular */
+`;
+
+export const Secondary = styled(ButtonCommon)`
+  padding: 0.7rem 1.6rem;
+
+  &:hover {
+    background: var(--gray300);
+  }
+
+  &:active {
+    background: var(--gray100);
+  }
+
+  &:focus {
+    border: 1px solid var(--gray500);
+  }
+`;
---
+++ src/components/common/Buttons/movePageButton.jsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import { Primary56 } from './ButtonStyle';
+
+function MovePageButton({ moveLink, btnName }) {
+  const navigate = useNavigate();
+  const handleClickButton = () => {
+    navigate(moveLink);
+  };
+
+  return <Button onClick={handleClickButton}>{btnName}</Button>;
+}
+const Button = styled(Primary56)`
+  width: 280px;
+  margin: 24px 24px;
+  cursor: pointer;
+
+  @media (max-width: 1248px) {
+    width: 100%;
+  }
+`;
+export default MovePageButton;
