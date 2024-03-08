373689edcd6c5e2c769c2d064ebb973830da3687
Youth-o <2021111495@dgu.ac.kr>
2024-03-08 13:35:45 +0900

    Feat: 프로필 이미지 최대 3개까지 불러오도록 컴포넌트 수정 및 스타일 적용

--- src/components/post/subheader/ProfileUser.jsx
+++ src/components/post/subheader/ProfileUser.jsx
@@ -1,12 +1,30 @@
 import styled from 'styled-components';
 
-function ProfileUser({ src }) {
+function ProfileUser({ src, last }) {
   const ProfileContainer = styled.div`
     display: flex;
+    position: relative;
+  `;
+
+  const ImageBox = styled.div`
+    width: 28px;
+    height: 28px;
+    margin-right: ${last ? '0' : '-24px'};
+
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 140px;
+      border: 1.4px solid #e3e3e3;
+      position: relative;
+      z-index: ${last ? '2' : '1'};
+    }
   `;
   return (
     <ProfileContainer>
-      <img src={src} alt="프로필 이미지" />
+      <ImageBox>
+        <img src={src} alt="프로필 이미지" />
+      </ImageBox>
     </ProfileContainer>
   );
 }
--- src/components/post/subheader/WrittenByIcons.jsx
+++ src/components/post/subheader/WrittenByIcons.jsx
@@ -1,17 +1,31 @@
 import ProfileUser from './ProfileUser';
 
 function WrittenByIcons({ messages }) {
+  if (!messages || messages.length === 0) {
+    return null; // Return early if messages are null or empty
+  }
+
+  const uniqueUsers = [...new Set(messages.map((message) => message.id))];
+  const limitedUsers = uniqueUsers.slice(0, 3);
+
   return (
     <>
       {/* {으앙~} */}
-      {messages &&
-        messages.map((message) => (
+      {limitedUsers.map((userId, index) => {
+        const userMessages = messages.filter(
+          (message) => message.id === userId,
+        );
+        const last = index === limitedUsers.length - 1;
+
+        return (
           <ProfileUser
-            key={message.id}
-            id={message.id}
-            src={message.profileImageURL}
+            key={userId}
+            id={userId}
+            src={userMessages[0].profileImageURL}
+            last={last}
           />
-        ))}
+        );
+      })}
     </>
   );
 }
