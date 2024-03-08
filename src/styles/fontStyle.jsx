256b24d8e8b0d9ec61d1a39d2fcd04b97f66f369
cgw-notebook <mangang0713@gmail.com>
2024-03-08 14:05:35 +0900

    Refactor: color, fontStyle 변경

---
+++ src/styles/fontStyle.jsx
@@ -0,0 +1,131 @@
+import styled from 'styled-components';
+
+const fontStyle = {
+  bold28: styled.css`
+    font-family: Pretendard;
+    font-size: 28px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 42px; /* 150% */
+    letter-spacing: -0.28px;
+  `,
+
+  bold24: styled.css`
+    font-family: Pretendard;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 42px; /* 150% */
+    letter-spacing: -0.28px;
+  `,
+
+  regular24: styled.css`
+    font-family: Pretendard;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 36px; /* 150% */
+    letter-spacing: -0.24px;
+  `,
+
+  bold20: styled.css`
+    font-family: Pretendard;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 30px; /* 150% */
+    letter-spacing: -0.2px;
+  `,
+
+  regular20: styled.css`
+    font-family: Pretendard;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 30px; /* 150% */
+    letter-spacing: -0.2px;
+  `,
+
+  bold18: styled.css`
+    font-family: Pretendard;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 28px; /* 150% */
+    letter-spacing: -0.18px;
+  `,
+
+  regular18: styled.css`
+    font-family: Pretendard;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 28px; /* 150% */
+    letter-spacing: -0.18px;
+  `,
+
+  bold16: styled.css`
+    font-family: Pretendard;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 26px; /* 150% */
+    letter-spacing: -0.16px;
+  `,
+
+  regular16: styled.css`
+    font-family: Pretendard;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 26px; /* 150% */
+    letter-spacing: -0.16px;
+  `,
+
+  regular15: styled.css`
+    font-family: Pretendard;
+    font-size: 15px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 22px; /* 150% */
+    letter-spacing: -0.15px;
+  `,
+
+  bold15: styled.css`
+    font-family: Pretendard;
+    font-size: 15px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 22px; /* 150% */
+    letter-spacing: -0.15px;
+  `,
+
+  regular14: styled.css`
+    font-family: Pretendard;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 20px; /* 150% */
+    letter-spacing: -0.07px;
+  `,
+
+  bold14: styled.css`
+    font-family: Pretendard;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 20px; /* 150% */
+    letter-spacing: -0.07px;
+  `,
+
+  regular12: styled.css`
+    font-family: Pretendard;
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 18px; /* 150% */
+    letter-spacing: -0.06px;
+  `,
+};
+
+export default fontStyle;
