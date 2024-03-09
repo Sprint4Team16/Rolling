ad0f8546d98c982a7b5f139ea75675cbb220718b
김경주 <popo505@naver.com>
2024-03-09 18:55:24 +0900

    Refactor: styled-components 오류 수정

--- src/components/post/subheader/ProfileUser.jsx
+++ src/components/post/subheader/ProfileUser.jsx
@@ -1,45 +1,45 @@
 import styled from 'styled-components';
 
-function ProfileUser({ src, last, peopleNum }) {
-  const ProfileContainer = styled.div`
-    display: flex;
-    position: relative;
-  `;
+const ProfileContainer = styled.div`
+  display: flex;
+  position: relative;
+`;
 
-  const ImageBox = styled.div`
-    width: 2.8rem;
-    height: 2.8rem;
-    margin-right: ${last ? '0' : '-2.4rem'};
+const ImageBox = styled.div`
+  width: 2.8rem;
+  height: 2.8rem;
+  margin-right: ${(props) => (props.last ? '0' : '-2.4rem')};
 
-    img {
-      width: 100%;
-      height: 100%;
-      border-radius: 14rem;
-      border: 0.14rem solid #e3e3e3;
-      position: relative;
-      z-index: ${last ? '2' : '1'};
-    }
-  `;
-
-  const RemainBox = styled.div`
-    width: 2.8rem;
-    height: 2.8rem;
-    margin-left: -1.2rem;
-    background: var(--white);
+  img {
+    width: 100%;
+    height: 100%;
     border-radius: 14rem;
     border: 0.14rem solid #e3e3e3;
-    z-index: 3;
-    text-align: center;
+    position: relative;
+    z-index: ${(props) => (props.last ? '2' : '1')};
+  }
+`;
 
-    p {
-      font-size: 1.2rem;
-      color: #484848;
-    }
-  `;
+const RemainBox = styled.div`
+  width: 2.8rem;
+  height: 2.8rem;
+  margin-left: -1.2rem;
+  background: var(--white);
+  border-radius: 14rem;
+  border: 0.14rem solid #e3e3e3;
+  z-index: 3;
+  text-align: center;
 
+  p {
+    font-size: 1.2rem;
+    color: #484848;
+  }
+`;
+
+function ProfileUser({ src, last, peopleNum }) {
   return (
     <ProfileContainer>
-      <ImageBox>
+      <ImageBox last={last ? 'true' : undefined}>
         <img src={src} alt="프로필 이미지" />
       </ImageBox>
       {peopleNum > 0 && (
