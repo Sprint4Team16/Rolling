333393a1739e2507367b4d36121f6cd29420f7fe
Hooni07 <gkxogns21@gmail.com>
2024-03-09 11:35:17 +0900

    Chore: 기준값 변경

--- src/constants/DISPLAY_SIZE.js
+++ src/constants/DISPLAY_SIZE.js
@@ -1,8 +1,8 @@
 export const DISPLAY_SIZE = Object.freeze({
   MAX_TABLET: 1247,
   MIN_TABLET: 1200,
-  MAX_MOBILE: 768,
-  MIN_MOBILE: 560,
+  MAX_MOBILE: 767,
+  MIN_MOBILE: 360,
 });
 
 export default DISPLAY_SIZE;
