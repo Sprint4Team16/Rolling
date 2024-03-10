24087da997457e1e899660b642b353bc59d3791d
Hooni07 <gkxogns21@gmail.com>
2024-03-10 13:23:25 +0900

    Refactor: PostId에 정의한 backgroundColors 상수화 및 적용

--- src/constants/COLOR_SET.js
+++ src/constants/COLOR_SET.js
@@ -6,3 +6,10 @@ export const USER_STATE = Object.freeze({
   지인: { background: 'var(--orange100)', color: 'var(--orange500)' },
   친구: { background: 'var(--blue100)', color: 'var(--blue500)' },
 });
+
+export const BACKGROUND_COLOR = Object.freeze({
+  beige: { background: 'var(--orange200)' },
+  purple: { background: 'var(--purple200)' },
+  green: { background: 'var(--green200)' },
+  blue: { background: 'var(--blue200)' },
+});
