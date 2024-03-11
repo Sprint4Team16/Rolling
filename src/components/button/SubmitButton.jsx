f771e87a4641b9383e3c031f81c969b701e93351
Hooni07 <79882248+Hooni07@users.noreply.github.com>
2024-03-11 11:47:32 +0900

    Refactor: PutPatchButton, PostButton을 공통 관리하는 SubmitButton 컴포넌트 생성 및 적용

--- src/components/button/SubmitButton.jsx
+++ src/components/button/SubmitButton.jsx
@@ -1,18 +1,24 @@
 import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { Primary56 } from '../../styles/ButtonStyle';
 
-function PutPatchButton({ onSubmit, btnDisable }) {
+function SubmitButton({ onSubmit, btnDisable, btnName }) {
   const navigate = useNavigate();
+  const location = useLocation();
+  const isEditRoute = location.pathname.includes('/edit');
 
   const handleButtonClick = async () => {
     const recipientId = await onSubmit();
-    navigate(`/post/${recipientId}/edit`, { replace: true });
+    if (isEditRoute) {
+      navigate(`/post/${recipientId}/edit`, { replace: true });
+    } else {
+      navigate(`/post/${recipientId}`, { replace: true });
+    }
   };
 
   return (
     <Button onClick={handleButtonClick} disabled={btnDisable}>
-      수정하기
+      {btnName}
     </Button>
   );
 }
@@ -25,4 +31,4 @@ const Button = styled(Primary56)`
   }
 `;
 
-export default PutPatchButton;
+export default SubmitButton;
