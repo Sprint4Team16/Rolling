c597bb56e35f936c237ce178a23c41b68d2d74f8
Hooni07 <79882248+Hooni07@users.noreply.github.com>
2024-03-11 12:47:23 +0900

    Refactor: EditMessageButton, EditButton을 공통 관리하는 EditButton 컴포넌트 생성 및 적용

--- src/components/button/EditButton.jsx
+++ src/components/button/EditButton.jsx
@@ -1,10 +1,22 @@
 import styled from 'styled-components';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { Primary56 } from '../../styles/ButtonStyle';
 
-function EditButton() {
+function TempEditButton({ messageId }) {
   const navigate = useNavigate();
+  const location = useLocation();
   const { id: recipientID } = useParams();
+  const isEditRoute = location.pathname.includes('/edit');
+
+  if (isEditRoute) {
+    return (
+      <ButtonAbsolute
+        onClick={() => navigate(`/post/${recipientID}/edit/${messageId}`)}
+      >
+        메세지 수정하기
+      </ButtonAbsolute>
+    );
+  }
 
   return (
     <Button onClick={() => navigate(`/post/${recipientID}/edit`)}>
@@ -15,4 +27,10 @@ function EditButton() {
 
 const Button = styled(Primary56)``;
 
-export default EditButton;
+const ButtonAbsolute = styled(Button)`
+  position: absolute;
+  top: 3.3rem;
+  right: 3.6rem;
+`;
+
+export default TempEditButton;
